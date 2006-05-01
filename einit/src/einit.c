68dcc542e6745c04fd2e7264d6d30e3f25cc7e61
mdeininger <mdeininger@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2006-05-01 14:25:17 +0000

    more ipc
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@141 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/src/einit.c
+++ einit/src/einit.c
@@ -44,10 +44,14 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <pthread.h>
+#include <errno.h>
+#include <string.h>
 
 int main(int, char **);
 int print_usage_info ();
 int switchmode (char *);
+int ipc_process (int *);
 int ipc_wait ();
 
 char *currentmode = "void";
@@ -113,11 +117,16 @@ int print_usage_info () {
  return -1;
 }
 
+int ipc_process (int *fd) {
+}
+
 int ipc_wait () {
  struct cfgnode *node = cfg_findnode ("control-socket", 0, NULL);
  int sock = socket (AF_UNIX, SOCK_STREAM, 0);
  struct sockaddr_un saddr;
  int nfd;
+ pthread_t **cthreads;
+ pthread_attr_t threadattr;
  if (sock == -1)
   return bitch (BTCH_ERRNO);
 
@@ -135,11 +144,56 @@ int ipc_wait () {
   return bitch (BTCH_ERRNO);
  }
 
+ pthread_attr_init (&threadattr);
+
+/* i was originally intending to create one thread per connection, but i think one thread in total should
+   be sufficcient */
  while ((nfd = accept (sock, NULL, NULL)) != -1) {
-  printf ("new connection: %i", nfd);
-  puts ("");
+//  pthread_t *thread = ecalloc (1, sizeof (pthread_t));
+//  pthread_create (thread, &threadattr, (void *(*)(void *))ipc_process, (void *)&nfd);
+//  pthread_detach (*thread);
+  ssize_t br;
+  ssize_t ic = 0;
+  ssize_t i;
+  char buf[BUFFERSIZE+1];
+  char lbuf[BUFFERSIZE+1];
+
+  while (br = read (nfd, buf, BUFFERSIZE)) {
+   if ((br < 0) && (errno != EAGAIN) && (errno != EINTR)) {
+    bitch (BTCH_ERRNO);
+    break;
+   }
+   for (i = 0; i < br; i++) {
+    if ((buf[i] == '\n') || (buf[i] == '\0')) {
+     lbuf[ic] = 0;
+     if (lbuf[0]) {
+      puts (lbuf);
+     }
+     ic = -1;
+     lbuf[0] = 0;
+	} else {
+     if (ic >= BUFFERSIZE) {
+      lbuf[ic] = 0;
+      if (lbuf[0]) {
+       puts (lbuf);
+      }
+      ic = 0;
+     }
+     lbuf[ic] = buf[i];
+    }
+    ic++;
+   }
+  }
+  lbuf[ic] = 0;
+  if (lbuf[0]) {
+   puts (lbuf);
+  }
   close (nfd);
+//  close (nfd);
  }
+
+ pthread_attr_destroy (&threadattr);
+
  if (nfd == -1)
   bitch (BTCH_ERRNO);
 
