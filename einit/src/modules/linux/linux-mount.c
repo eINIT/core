b72f7ff11b13627d0bec295ce474461c748d39d7
mdeininger <mdeininger@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2006-06-08 09:44:48 +0000

    moved around some functions from linux-mount to common-mount
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@198 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/src/modules/linux/linux-mount.c
+++ einit/src/modules/linux/linux-mount.c
@@ -37,17 +37,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define _MODULE
 
-#include <stdio.h>
 #include <einit/module.h>
 #include <einit/config.h>
-#include <einit/utility.h>
-#include <einit/bitch.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <pthread.h>
+#include <einit/common-mount.h>
 
 #define EXPECTED_EIV 1
 
@@ -55,19 +47,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #warning "This module was developed for a different version of eINIT, you might experience problems"
 #endif
 
-/* struct definitions */
-struct bd_info {
- char *label, *uuid, *fs;
-};
-
-struct fstab_entry {
- char *mountpoint, *device, *fs;
- char **options;
- uint32_t mountflags;
-};
-
 /* variable definitions */
-pthread_mutex_t blockdevices_mutex = PTHREAD_MUTEX_INITIALIZER;
 char *defaultblockdevicesource[] = {"dev", NULL};
 char *defaultfstabsource[] = {"label", "configuration", "fstab", NULL};
 struct uhash *blockdevices = NULL;
@@ -109,7 +89,6 @@ struct smodule sm_root = {
 };
 
 /* function declarations */
-void find_block_devices_recurse_path (char *);
 void update_block_devices (void);
 void update_fstab (void);
 int scanmodules (struct lmodule *);
@@ -119,43 +98,6 @@ int enable (void *, struct mfeedback *);
 int disable (void *, struct mfeedback *);
 
 /* function definitions */
-void find_block_devices_recurse_path (char *path) {
- DIR *dir;
- struct dirent *entry;
-
- dir = opendir (path);
- if (dir != NULL) {
-  while (entry = readdir (dir)) {
-   if (entry->d_name[0] == '.') continue;
-   struct stat statbuf;
-   char *tmp = ecalloc (1, strlen(path) + entry->d_reclen + 1);
-   tmp = strcat (tmp, path);
-   tmp = strcat (tmp, entry->d_name);
-   if (lstat (tmp, &statbuf)) {
-    perror ("einit-linux-mount");
-    free (tmp);
-    continue;
-   }
-   if (S_ISDIR (statbuf.st_mode)) {
-    tmp = strcat (tmp, "/");
-    find_block_devices_recurse_path (tmp);
-   }
-
-   if (S_ISBLK (statbuf.st_mode) && !S_ISLNK(statbuf.st_mode)) {
-    pthread_mutex_lock (&blockdevices_mutex);
-    blockdevices = hashadd (blockdevices, tmp, NULL);
-    pthread_mutex_unlock (&blockdevices_mutex);
-   } else
-    free (tmp);
-  }
-  closedir (dir);
- } else {
-  fprintf (stderr, "einit-linux-mount: could not open %s\n", path);
-  errno = 0;
-  return;
- }
-}
-
 void update_block_devices (void) {
  struct cfgnode *node = cfg_findnode ("mount-block-devices-source", 0, NULL);
  char **bds = defaultblockdevicesource;
