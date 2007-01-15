ac80a3fc3abc6e6cf7d6b27aa2c7a73ac62054ff
mdeininger <mdeininger@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-01-15 21:57:13 +0000

    typo
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@596 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/src/modules/linux/linux-sysconf.c
+++ einit/src/modules/linux/linux-sysconf.c
@@ -64,7 +64,7 @@ const struct smodule self = {
 	.rid		= "linux-sysconf",
     .si           = {
         .provides = provides,
-        .requires = NULL",
+        .requires = NULL,
         .after    = NULL,
         .before   = NULL
     }
