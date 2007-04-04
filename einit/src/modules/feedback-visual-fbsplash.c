bc9323c8e80b14527addeadcfa4c7124e3118579
mdeininger <mdeininger@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-04-04 19:54:38 +0000

    added a module for the splash daemon (gonna be needed for the fbsplash feedback module)
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@1056 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/src/modules/feedback-visual-fbsplash.c
+++ einit/src/modules/feedback-visual-fbsplash.c
@@ -48,7 +48,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
-#include <einit-modules/exec.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -64,6 +63,7 @@ int _einit_feedback_visual_fbsplash_configure (struct lmodule *);
 #if defined(_EINIT_MODULE) || defined(_EINIT_MODULE_HEADER)
 
 char *provides[] = {"feedback-visual", "feedback-graphical", NULL};
+char *requires[] = {"splashd", NULL};
 const struct smodule _einit_feedback_visual_fbsplash_self = {
  .eiversion = EINIT_VERSION,
  .eibuild   = BUILDNUMBER,
@@ -85,226 +85,12 @@ module_register(_einit_feedback_visual_fbsplash_self);
 
 #endif
 
-struct planref {
- struct mloadplan *plan;
- time_t startedat;
- uint32_t max_changes;
- uint32_t min_changes;
-};
-
-struct lmodule *self_l = NULL;
-
-void einit_event_handler(struct einit_event *);
-void feedback_event_handler(struct einit_event *);
-
-char *splash_functions = "/sbin/splash-functions.sh";
-char *scriptlet_action = NULL;
-
-struct planref **plans = NULL;
-pthread_mutex_t plansmutex = PTHREAD_MUTEX_INITIALIZER;
-
-void ipc_event_handler (struct einit_event *ev) {
- if (ev && ev->set && ev->set[0] && ev->set[1] && !strcmp(ev->set[0], "examine") && !strcmp(ev->set[1], "configuration")) {
-  if (!cfg_getstring("configuration-feedback-visual-fbsplash-splash-functions", NULL)) {
-   fputs (" * configuration variable \"configuration-feedback-visual-fbsplash-splash-functions\" not found : fbsplash support disabled.\n", (FILE *)ev->para);
-   ev->task++;
-  }
-  if (!cfg_getnode("configuration-feedback-visual-fbsplash-scriptlets", NULL)) {
-   fputs (" * configuration variable \"configuration-feedback-visual-fbsplash-scriptlets\" not found : fbsplash support disabled.\n", (FILE *)ev->para);
-   ev->task++;
-  }
-
-  ev->flag = 1;
- }
+int _einit_feedback_visual_fbsplash_cleanup (struct lmodule *tm) {
 }
 
-int configure (struct lmodule *this) {
- exec_configure (this);
- char *s = NULL;
+int _einit_feedback_visual_fbsplash_configure (struct lmodule *tm) {
+ module_init (tm);
 
- struct cfgnode *node;
+ tm->cleanup = _einit_feedback_visual_fbsplash_cleanup;
 
- if (s = cfg_getstring("configuration-feedback-visual-fbsplash-splash-functions", NULL))
-  splash_functions = s;
-
- self_l = this;
- event_listen (EVENT_SUBSYSTEM_IPC, ipc_event_handler);
-}
-
-int cleanup (struct lmodule *this) {
- exec_cleanup(this);
- event_ignore (EVENT_SUBSYSTEM_IPC, ipc_event_handler);
-}
-
-int enable (void *pa, struct einit_event *status) {
- char *s = NULL;
-
- struct stat st;
- if (stat (splash_functions, &st)) return STATUS_FAIL;
-
- if (s = cfg_getstring("configuration-feedback-visual-fbsplash-scriptlets/action", NULL)) {
-  scriptlet_action = s;
- } else
-  return STATUS_FAIL;
-
-// remember: pexec(command, variables, uid, gid, user, group, local_environment, status)
-
-  if (s = cfg_getstring("configuration-feedback-visual-fbsplash-scriptlets/init", NULL)) {
-   char *vars[] = { "splash-functions-file", splash_functions, "new-mode", "default", NULL },
-        *subs = apply_variables (s, vars);
-   int ret = STATUS_FAIL;
-
-   if (subs) {
-    ret = pexec(subs, NULL, 0, 0, NULL, NULL, NULL, status);
-
-    free (subs);
-   }
-
-   if (ret & STATUS_OK) {
-    event_listen (EVENT_SUBSYSTEM_EINIT, einit_event_handler);
-    event_listen (EVENT_SUBSYSTEM_FEEDBACK, feedback_event_handler);
-   }
-
-   return ret;
-  }
-
-  return STATUS_FAIL;
-}
-
-int disable (void *pa, struct einit_event *status) {
- char *s = NULL;
-
- pthread_mutex_lock (&self_l->imutex);
- event_ignore (EVENT_SUBSYSTEM_FEEDBACK, feedback_event_handler);
-
- if (s = cfg_getstring("configuration-feedback-visual-fbsplash-scriptlets/quit", NULL)) {
-  char *vars[] = { "splash-functions-file", splash_functions, "new-mode", "default", NULL },
-  *subs = apply_variables (s, vars);
-  int ret = STATUS_FAIL;
-
-  if (subs) {
-   ret = pexec(subs, NULL, 0, 0, NULL, NULL, NULL, status);
-
-   free (subs);
-  }
-
-  if (ret & STATUS_OK) {
-   event_ignore (EVENT_SUBSYSTEM_FEEDBACK, feedback_event_handler);
-   event_ignore (EVENT_SUBSYSTEM_EINIT, einit_event_handler);
-  }
-
-  pthread_mutex_unlock (&self_l->imutex);
-
-  return ret;
- }
-
- pthread_mutex_unlock (&self_l->imutex);
- return STATUS_FAIL;
-}
-
-void feedback_event_handler(struct einit_event *ev) {
- pthread_mutex_lock (&self_l->imutex);
-
- if (ev->type == EVE_FEEDBACK_PLAN_STATUS) {
-  int i = 0;
-  struct planref plan, *cul = NULL;
-  uint32_t startedat = 0;
-  switch (ev->task) {
-   case MOD_SCHEDULER_PLAN_COMMIT_START:
-    pthread_mutex_lock (&plansmutex);
-    plan.plan = (struct mloadplan *)ev->para;
-    plan.startedat = time (NULL);
-    plan.max_changes = 0;
-    plan.min_changes = 0;
-    plans = (struct planref **)setadd ((void **)plans, (void *)&plan, sizeof (struct planref));
-    pthread_mutex_unlock (&plansmutex);
-    break;
-   case MOD_SCHEDULER_PLAN_COMMIT_FINISH:
-    if (!plans) break;
-    pthread_mutex_lock (&plansmutex);
-    for (; plans[i]; i++)
-     if (plans[i]->plan == (struct mloadplan *)ev->para) {
-     cul = plans[i];
-     startedat = plans[i]->startedat;
-     break;
-     }
-     if (cul)
-      plans = (struct planref **)setdel ((void **)plans, (void *)cul);
-     pthread_mutex_unlock (&plansmutex);
-  }
- }
-
- pthread_mutex_unlock (&self_l->imutex);
- return;
-}
-
-void einit_event_handler(struct einit_event *ev) {
- pthread_mutex_lock (&self_l->imutex);
-
- if (ev->type == EVE_SERVICE_UPDATE) {
-  uint32_t u = 0;
-  if (ev->set) for (u = 0; ev->set[u]; u++) {
-   if (plans) {
-    uint32_t i = 0;
-    char  *nmode = "unknown",
-         **all_services = NULL,
-         **active_services = NULL,
-         *splash_command[] = { "splash",
-                               (ev->task & MOD_ENABLE) ?
-                                 ((ev->status == STATUS_WORKING) ? "svc_start" : ((ev->status & STATUS_OK) ? "svc_started" : "svc_stopped")) :
-                                 ((ev->status == STATUS_WORKING) ? "svc_stop" : ((ev->status & STATUS_OK) ? "svc_stopped" : "svc_started")),
-                               ev->set[u], NULL };
-
-    pthread_mutex_lock (&plansmutex);
-
-    for (; plans[i]; i++) {
-     if (plans[i]->plan) {
-      if (plans[i]->plan) {
-       if (plans[i]->plan->mode && plans[i]->plan->mode->id) nmode = plans[i]->plan->mode->id;
-
-       if (plans[i]->plan->services) {
-        struct stree *ha = plans[i]->plan->services;
-        while (ha) {
-         all_services = (char **)setadd ((void **)all_services, (void *)ha->key, SET_TYPE_STRING);
-
-         if (((struct mloadplan_node *)(ha->value))->changed == 2) {
-          active_services = (char **)setadd ((void **)active_services, (void *)ha->key, SET_TYPE_STRING);
-         }
-
-         ha = streenext(ha);
-        }
-       }
-      }
-     }
-    }
-
-    if (all_services && splash_command) {
-     char *as = set2str (' ', all_services),
-          *acs = set2str (' ', active_services),
-          *cm = set2str (' ', splash_command),
-          *var[] = { "splash-functions-file", splash_functions,
-                     "new-mode", nmode,
-                     "all-services", as ? as : "none",
-                     "active-services", acs ? acs : "none",
-                     "action", cm ? cm : "/bin/false",
-                     NULL },
-          *s = apply_variables(scriptlet_action, var);
-
-
-     if (s) pexec(s, NULL, 0, 0, NULL, NULL, NULL, NULL);
-     if (as) free (as);
-     if (acs) free (acs);
-     if (cm) free (cm);
-    }
-
-    if (all_services) free (all_services);
-    if (active_services) free (active_services);
-
-    pthread_mutex_unlock (&plansmutex);
-   }
-  }
- }
-
- pthread_mutex_unlock (&self_l->imutex);
- return;
 }
