87f261835fe0c750088d9aab8c31e50826f887c4
mdeininger <mdeininger@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-04-06 17:56:58 +0000

    preparing for new network modules...
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@1093 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/src/modules/network.c
+++ einit/src/modules/network.c
@@ -56,9 +56,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define IF_OK          0x1
 
 int _network_scanmodules (struct lmodule *);
-int _network_interface_enable (void *, struct einit_event *);
-int _network_interface_disable (void *, struct einit_event *);
-int _network_interface_custom (void *, char *, struct einit_event *);
+int _network_interface_enable (struct cfgnode *, struct einit_event *);
+int _network_interface_disable (struct cfgnode *, struct einit_event *);
+int _network_interface_custom (struct cfgnode *, char *, struct einit_event *);
+int _network_interface_cleanup (struct lmodule *);
+int _network_interface_configure (struct lmodule *);
 int _network_cleanup (struct lmodule *);
 int _network_configure (struct lmodule *);
 
@@ -106,23 +108,42 @@ int _network_scanmodules (struct lmodule *mainlist) {
 
     if (nn->arbattrs) {
      char *interfacename = cur->key+33; /* 33 = strlen("configuration-network-interfaces-") */
-     uint32_t i = 0;
      struct smodule newmodule;
      char tmp[BUFFERSIZE];
+     struct lmodule *lm = mainlist;
 
      memset (&newmodule, 0, sizeof (struct smodule));
-     esprintf (tmp, BUFFERSIZE, "net-%s", interfacename);
 
-//     newmodule.rid = estrdup (tmp);
+     while (lm) {
+      if (lm->source && strmatch(lm->source, cur->key)) {
+       lm->param = NULL;
 
-     for (; nn->arbattrs[i]; i+=2) {
-      if (strmatch (nn->arbattrs[i], "ip")) {
-      } else if (strmatch(nn->arbattrs[i], "control")) {
-      } else if (strmatch(nn->arbattrs[i], "kernel-module")) {
+       goto do_next;
       }
+
+      lm = lm->next;
      }
+
+     newmodule.configure = _network_interface_configure;
+     newmodule.rid = estrdup (cur->key);
+     newmodule.eiversion = EINIT_VERSION;
+     newmodule.eibuild = BUILDNUMBER;
+     newmodule.version = 1;
+     newmodule.mode = EINIT_MOD_EXEC;
+     newmodule.options = 0;
+
+     esprintf (tmp, BUFFERSIZE, "Network Interface (%s)", interfacename);
+     newmodule.name = estrdup (tmp);
+
+     esprintf (tmp, BUFFERSIZE, "net-%s", interfacename);
+     newmodule.si.provides = (char **)setadd ((void **)newmodule.si.provides, (void *)tmp, SET_TYPE_STRING);
+
+     lm = mod_add (NULL, &newmodule);
+     lm->source = estrdup (cur->key);
     }
    }
+
+   do_next:
 
    cur = streenext (cur);
   }
@@ -133,20 +154,36 @@ int _network_scanmodules (struct lmodule *mainlist) {
  return 0;
 }
 
-int _network_interface_enable (void *p, struct einit_event *status) {
+int _network_interface_enable (struct cfgnode *nn, struct einit_event *status) {
+ return STATUS_FAIL;
 }
 
-int _network_interface_disable (void *p, struct einit_event *status) {
+int _network_interface_disable (struct cfgnode *nn, struct einit_event *status) {
+ return STATUS_FAIL;
 }
 
-int _network_interface_custom (void *p, char *action, struct einit_event *status) {
+int _network_interface_custom (struct cfgnode *nn, char *action, struct einit_event *status) {
+ return STATUS_FAIL;
 }
 
 int _network_cleanup (struct lmodule *this) {
  return 0;
 }
 
-int _network_cleanup_after_module (struct lmodule *this) {
+int _network_interface_cleanup (struct lmodule *this) {
+ return 0;
+}
+
+int _network_interface_configure (struct lmodule *this) {
+ if (!this->module || !this->module->rid) return 1;
+
+ this->cleanup = _network_interface_cleanup;
+ this->enable = (int (*)(void *, struct einit_event *))_network_interface_enable;
+ this->disable = (int (*)(void *, struct einit_event *))_network_interface_disable;
+ this->custom = (int (*)(void *, char *, struct einit_event *))_network_interface_custom;
+
+ this->param = NULL;
+
  return 0;
 }
 
@@ -154,7 +191,7 @@ int _network_configure (struct lmodule *this) {
  module_init(this);
 
  thismodule->cleanup = _network_cleanup;
- thismodule->scanmodules = _network_scanmodules;
+// thismodule->scanmodules = _network_scanmodules;
 
 #if 0
  event_listen (EVENT_SUBSYSTEM_EINIT, network_einit_event_handler);
