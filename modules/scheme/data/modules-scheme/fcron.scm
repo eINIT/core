4d87dc8d4084a2ad3d0f24a17a199358482e94fd
jyujin <jyujin@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-06-09 08:41:03 +0000

    include both *.scheme and *.scm; the dummy module was probably quite weird to look at, i modified the fcron module so that it oughta work :)
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@1600 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- modules/scheme/data/modules-scheme/fcron.scm
+++ modules/scheme/data/modules-scheme/fcron.scm
@@ -1,7 +1,16 @@
 ;;;; FCron Scheme Module
+(define (fcron-enable)
+  (pexec "/usr/sbin/fcron -b -c /etc/fcron/fcron.conf")
+
+(define (fcron-disable)
+  (pexec /bin/killall fcron")
+
+; or (this one should be equivalent to the definition above):
+; (define fcron-disable
+;   (lambda ()
+;      (pexec /bin/killall fcron")))
+
 (make-module "scheme-fcron" "Cron (fcron)"
-	     '(("cron" "fcron") ("mount-critical"))
-	     '("enable" . fcron-enable)
-	     '("disable" . fcron-disable)
-	     (cons "fcron-enable" (lambda () (pexec "/usr/sbin/fcron -b -c /etc/fcron/fcron.conf")))
-	     (cons "fcron-disable" (lambda () (pexec "/bin/killall fcron"))))
+  '(("cron" "fcron") ("mount-critical"))
+  '("enable" . fcron-enable)
+  '("disable" . fcron-disable))
