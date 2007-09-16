e886ab4a87c7aec44eb0846bf4eb139de5ab1523
jyujin <jyujin@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-09-16 16:42:24 +0000

    incomplete, but sorta working new module-xml loader
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@2039 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/src/modules/module-xml.c
+++ einit/src/modules/module-xml.c
@@ -45,6 +45,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <einit/utility.h>
 #include <errno.h>
 
+#include <einit-modules/exec.h>
+
 #define EXPECTED_EIV 1
 
 #if EXPECTED_EIV != EINIT_VERSION
@@ -75,23 +77,150 @@ module_register(module_xml_v2_self);
 
 #endif
 
+#define MODULES_PREFIX "services-virtual-module-"
+#define MODULES_PREFIX_LENGTH (sizeof(MODULES_PREFIX) -1)
+
+#define MODULES_EXECUTE_NODE_TEMPLATE MODULES_PREFIX "%s-execute"
+
+struct stree *module_xml_v2_modules = NULL;
+
 int module_xml_v2_scanmodules (struct lmodule *);
 
 int module_xml_v2_cleanup (struct lmodule *pa) {
- /* cleanup code here */
+ exec_cleanup (pa);
 
  return 0;
 }
 
+int module_xml_v2_module_custom_action (char *name, char *action, struct einit_event *status) {
+ if (name && action) {
+  char buffer[BUFFERSIZE];
+  struct cfgnode *node = NULL;
+
+  esprintf (buffer, BUFFERSIZE, MODULES_EXECUTE_NODE_TEMPLATE, name);
+
+  while ((node = cfg_findnode (buffer, 0, node))) {
+   if (node->idattr && strmatch (node->idattr, action)) {
+    int x = 0;
+    char *code = NULL, *variables = NULL, *user = NULL, *group = NULL;
+
+    for (; node->arbattrs[x]; x+=2) {
+     if (strmatch (node->arbattrs[x], "code")) code = node->arbattrs[x+1];
+     else if (strmatch (node->arbattrs[x], "variables")) variables = node->arbattrs[x+1];
+     else if (strmatch (node->arbattrs[x], "user")) user = node->arbattrs[x+1];
+     else if (strmatch (node->arbattrs[x], "group")) group = node->arbattrs[x+1];
+    }
+
+    if (code) {
+     if (variables) {
+      char **split_variables;
+      int result;
+
+      split_variables = str2set (':', variables);
+
+      result = pexec (code, (const char **)split_variables, 0, 0, user, group, NULL, status);
+
+      free (split_variables);
+
+      return result;
+     } else
+      return pexec (code, NULL, 0, 0, user, group, NULL, status);
+    } else
+     return status_failed;
+   }
+  }
+ }
+
+ return status_failed;
+}
+
+int module_xml_v2_module_enable (char *name, struct einit_event *status) {
+ return module_xml_v2_module_custom_action (name, "enable", status);
+}
+
+int module_xml_v2_module_disable (char *name, struct einit_event *status) {
+ return module_xml_v2_module_custom_action (name, "disable", status);
+}
+
 int module_xml_v2_module_configure (struct lmodule *pa) {
+ pa->param = pa->module->rid;
+
+ pa->enable = (int (*)(void *, struct einit_event *))module_xml_v2_module_enable;
+ pa->disable = (int (*)(void *, struct einit_event *))module_xml_v2_module_disable;
+ pa->custom = (int (*)(void *, char *, struct einit_event *))module_xml_v2_module_custom_action;
+
+ module_xml_v2_modules = streeadd (module_xml_v2_modules, pa->module->rid, pa, SET_NOALLOC, NULL);
 
  return 0;
 }
 
 int module_xml_v2_scanmodules (struct lmodule *modchain) {
- struct stree *module_nodes = cfg_prefix("services-virtual-module-");
+ struct stree *modules_to_update = module_xml_v2_modules;
+
+ while (modules_to_update) {
+  mod_update (modules_to_update->value);
+
+  modules_to_update = streenext (modules_to_update);
+ }
+
+ struct stree *module_nodes = cfg_prefix(MODULES_PREFIX);
 
  if (module_nodes) {
+  struct stree *cur = module_nodes;
+
+  for (; cur; cur = streenext (cur)) {
+/* exclude legacy nodes */
+   if (strcmp (cur->key, MODULES_PREFIX "shell") && strcmp (cur->key, MODULES_PREFIX "daemon")) {
+    struct cfgnode *node = cur->value;
+    notice (1, "processing id=%s", cur->key + MODULES_PREFIX_LENGTH);
+
+    if ((!module_xml_v2_modules || !streefind (module_xml_v2_modules, cur->key + MODULES_PREFIX_LENGTH, tree_find_first)) && node->arbattrs) {
+     int i = 0;
+     char *name = NULL, *requires = NULL, *provides = NULL, *after = NULL, *before = NULL;
+
+     for (; node->arbattrs[i]; i+=2) {
+      if (strmatch (node->arbattrs[i], "name")) {
+       name = node->arbattrs[i+1];
+      } else if (strmatch (node->arbattrs[i], "provides")) {
+       provides = node->arbattrs[i+1];
+      } else if (strmatch (node->arbattrs[i], "requires")) {
+       requires = node->arbattrs[i+1];
+      } else if (strmatch (node->arbattrs[i], "after")) {
+       after = node->arbattrs[i+1];
+      } else if (strmatch (node->arbattrs[i], "before")) {
+       before = node->arbattrs[i+1];
+      }
+     }
+
+     if (name && provides) {
+      struct smodule *new_sm = emalloc (sizeof (struct smodule));
+      memset (new_sm, 0, sizeof (struct smodule));
+
+      new_sm->rid = estrdup (cur->key + MODULES_PREFIX_LENGTH);
+      new_sm->name = estrdup (name);
+
+      new_sm->eiversion = EINIT_VERSION;
+      new_sm->eibuild = BUILDNUMBER;
+      new_sm->version = 1;
+      new_sm->mode = einit_module_generic;
+
+      new_sm->si.provides = str2set (':', provides);
+      if (requires) new_sm->si.requires = str2set (':', requires);
+      if (after) new_sm->si.after = str2set (':', after);
+      if (before) new_sm->si.before = str2set (':', before);
+
+      new_sm->configure = module_xml_v2_module_configure;
+
+      mod_add (NULL, new_sm);
+
+      continue;
+     }
+    }
+   }
+
+  }
+
+  streefree (module_nodes);
  }
 
  return 1;
@@ -99,11 +228,10 @@ int module_xml_v2_scanmodules (struct lmodule *modchain) {
 
 int module_xml_v2_configure (struct lmodule *pa) {
  module_init (pa);
+ exec_configure (pa);
 
  pa->scanmodules = module_xml_v2_scanmodules;
  pa->cleanup = module_xml_v2_cleanup;
-
- /* more configure code here */
 
  return 0;
 }
