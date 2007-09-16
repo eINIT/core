5317ddb9e036b31e4e2eb9bff4b722795cc8c774
rmh3093 <rmh3093@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-09-16 16:01:56 +0000

    fixed device-mapper.sh
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@2038 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- modules/xml/data/modules-xml/device-mapper.sh
+++ modules/xml/data/modules-xml/device-mapper.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 if [ $1 = "enable" ]; then
-lvm-start.sh;
-fi
-
+./lvm-start.sh;
+ 
 elif  [ $1 = "disable" ]; then
-lvm-stop.sh;
-fi
-
+./lvm-stop.sh;
+ 
 elif [ $1 = "on-shutdown" ]; then
-lvm-stop.sh;
+./lvm-stop.sh;
 fi
