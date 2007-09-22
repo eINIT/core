214ef2c8fc96b9bffed9f242c79c7b3cbae3ff78
jyujin <jyujin@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-09-22 08:28:48 +0000

    minor cleanup
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@2091 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/data/scripts/update_conf.d.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for conf in `ls /etc/einit/conf.d`; do 
- differ=`diff -q /etc/einit/conf.d/${conf} ../conf.d/${conf} | grep -c differ`;
- if [ ${differ} != 0 ]; then 
-  diff -u /etc/einit/conf.d/${conf} ../conf.d/${conf} > /tmp/.update-${conf}.diff;
- fi;
-done
