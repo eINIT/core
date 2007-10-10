f2ea80e84c3cacbe81112a88e3b4d13c0a78b594
rmh3093 <rmh3093@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-10-10 05:43:16 +0000

    maybe it is working
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@2213 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- modules/xml/data/modules-xml/mdev.sh
+++ modules/xml/data/modules-xml/mdev.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-# eINIT udev script
-# original script (before breaking it out of udev.xml) written by Ryan Hope
-
-# pexec-options don't work here, so i had to make sure all udev apps get
-# something as their stdin. /dev/null should be appropriate.
+# eINIT mdev script
 
 if [ $1 = "enable" ]; then
  mount -n -t tmpfs -o exec,nosuid,mode=0755 mdev /dev
