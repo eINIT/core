afbb40bb78c28fe70a0fe990f5a8e8d08fd4c771
jyujin <jyujin@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-10-11 08:22:09 +0000

    made it so that storage modules load with udev, everything else after that; disabled kernel module unloading on shutdown -- that's not gonna help in any way, other than lock us up for buggy drivers
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@2216 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/src/modules/linux/linux-kernel-modules.c
+++ einit/src/modules/linux/linux-kernel-modules.c
@@ -102,28 +102,6 @@ int linux_kernel_modules_load (char **modules) {
  return status_ok;
 }
 
-int linux_kernel_modules_unload (char **modules) {
- if (!modules) return status_failed;
- char *modprobe_command = cfg_getstring ("configuration-command-rmmod/with-env", 0);
- uint32_t i = 0;
-
- for (; modules[i]; i++) {
-  const char *tpldata[] = { "module", modules[i], NULL };
-  char *applied = apply_variables (modprobe_command, tpldata);
-
-  if (applied) {
-   notice (4, "unloading kernel module: %s", modules[i]);
-
-   if (pexec (applied, NULL, 0, 0, NULL, NULL, NULL, NULL) & status_failed) {
-    notice (2, "unloading kernel module \"%s\" failed", modules[i]);
-   }
-  }
- }
-
- free (modules);
- return status_ok;
-}
-
 char **linux_kernel_modules_autoload_d() {
  char **rv = NULL;
  char *file = cfg_getstring ("configuration-kernel-modules-autoload.d/file", NULL);
@@ -151,9 +129,86 @@ char **linux_kernel_modules_autoload_d() {
  return rv;
 }
 
-int linux_kernel_modules_run (char shutdown) {
+enum lkm_run_code {
+ lkm_pre_dev,
+ lkm_post_dev,
+ lkm_shutdown
+};
+
+#define MPREFIX "configuration-kernel-modules-"
+
+char **linux_kernel_modules_get_from_node (char *node, char *dwait) {
+ int len = strlen (node) + sizeof(MPREFIX) + 1;
+ char *buffer = emalloc (len);
+ struct cfgnode *n;
+
+ esprintf (buffer, len, MPREFIX "%s", node);
+
+ n = cfg_getnode (buffer, NULL);
+ if (n) {
+  *dwait = !n->flag;
+  return str2set (':', n->svalue);
+ }
+
+ return NULL;
+}
+
+#define KERNEL_MODULES_PATH_PREFIX "/lib/modules/"
+
+char **linux_kernel_modules_storage() {
+ return NULL;
+}
+
+char **linux_kernel_modules_sound() {
+ return NULL;
+}
+
+char **linux_kernel_modules_get_by_subsystem (char *subsystem, char *dwait) {
+ char **rv = NULL;
+
+ if ((rv = linux_kernel_modules_get_from_node (subsystem, dwait))) {
+  return rv;
+ } else if (strmatch (subsystem, "generic") || strmatch (subsystem, "arbitrary")) {
+  *dwait = 1;
+  return linux_kernel_modules_autoload_d();
+ } else if (strmatch (subsystem, "storage")) {
+  *dwait = 1;
+  return linux_kernel_modules_storage();
+ } else if (strmatch (subsystem, "alsa") || strmatch (subsystem, "audio") || strmatch (subsystem, "sound")) {
+  *dwait = 1;
+  return linux_kernel_modules_sound();
+ }
+
+ return NULL;
+}
+
+#if 0
+
+int linux_kernel_modules_unload (char **modules) {
+ if (!modules) return status_failed;
+ char *modprobe_command = cfg_getstring ("configuration-command-rmmod/with-env", 0);
+ uint32_t i = 0;
+
+ for (; modules[i]; i++) {
+  const char *tpldata[] = { "module", modules[i], NULL };
+  char *applied = apply_variables (modprobe_command, tpldata);
+
+  if (applied) {
+   notice (4, "unloading kernel module: %s", modules[i]);
+
+   if (pexec (applied, NULL, 0, 0, NULL, NULL, NULL, NULL) & status_failed) {
+    notice (2, "unloading kernel module \"%s\" failed", modules[i]);
+   }
+  }
+ }
+
+ free (modules);
+ return status_ok;
+}
+
+int linux_kernel_modules_run (enum lkm_run_code code) {
  pthread_t **threads = NULL;
- struct stree *linux_kernel_modules_nodes = cfg_prefix("configuration-kernel-modules-");
+ struct stree *linux_kernel_modules_nodes = cfg_prefix(MPREFIX);
 
  if (linux_kernel_modules_nodes) {
   struct stree *cur = linux_kernel_modules_nodes;
@@ -164,7 +219,7 @@ int linux_kernel_modules_run (char shutdown) {
     char **modules = str2set (':', node->svalue);
 
     if (modules) {
-     if (shutdown) {
+     if (code == lkm_shutdown) {
       pthread_t *threadid = emalloc (sizeof (pthread_t));
 
       if (ethread_create (threadid, NULL, (void *(*)(void *))linux_kernel_modules_unload, modules)) {
@@ -194,7 +249,7 @@ int linux_kernel_modules_run (char shutdown) {
 
  char **kamodules = linux_kernel_modules_autoload_d();
  if (kamodules) {
-  if (shutdown) {
+  if (code == lkm_shutdown) {
    pthread_t *threadid = emalloc (sizeof (pthread_t));
 
    if (ethread_create (threadid, NULL, (void *(*)(void *))linux_kernel_modules_unload, kamodules)) {
@@ -229,16 +284,130 @@ int linux_kernel_modules_run (char shutdown) {
 
 void linux_kernel_modules_power_event_handler (struct einit_event *ev) {
  if ((ev->type == einit_power_down_imminent) || (ev->type == einit_power_reset_imminent)) {
-  linux_kernel_modules_run(1);
+  linux_kernel_modules_run(lkm_shutdown);
  }
 }
 
+#else
+
+int linux_kernel_modules_run (enum lkm_run_code code) {
+ pthread_t **threads = NULL;
+
+ if (code == lkm_pre_dev) {
+  char dwait;
+  char **modules = linux_kernel_modules_get_by_subsystem ("storage", &dwait);
+
+  if (modules) {
+   pthread_t *threadid = emalloc (sizeof (pthread_t));
+
+   if (ethread_create (threadid, NULL, (void *(*)(void *))linux_kernel_modules_load, modules)) {
+    linux_kernel_modules_load (modules);
+   } else {
+    if (dwait)
+     threads = (pthread_t **)setadd ((void **)threads, threadid, SET_NOALLOC);
+   }
+  }
+ } else if (code == lkm_post_dev) {
+  struct stree *linux_kernel_modules_nodes = cfg_prefix(MPREFIX);
+  char have_generic = 0;
+  char have_audio = 0;
+
+  if (linux_kernel_modules_nodes) {
+   struct stree *cur = linux_kernel_modules_nodes;
+
+   while (cur) {
+    char *subsystem = cur->key + sizeof (MPREFIX);
+
+    if (strmatch (subsystem, "storage")) {
+    } else {
+     struct cfgnode *node = cur->value;
+
+     if (strmatch (subsystem, "generic") || strmatch (subsystem, "arbitrary")) {
+      have_generic = 1;
+     } else if (strmatch (subsystem, "alsa") || strmatch (subsystem, "audio") || strmatch (subsystem, "sound")) {
+      have_audio = 1;
+     }
+
+     if (node && node->svalue) {
+      char **modules = str2set (':', node->svalue);
+
+      if (modules) {
+       pthread_t *threadid = emalloc (sizeof (pthread_t));
+
+       if (ethread_create (threadid, NULL, (void *(*)(void *))linux_kernel_modules_load, modules)) {
+        linux_kernel_modules_load (modules);
+       } else {
+        if (!node->flag)
+         threads = (pthread_t **)setadd ((void **)threads, threadid, SET_NOALLOC);
+       }
+      }
+     }
+    }
+
+    cur = streenext (cur);
+   }
+
+   streefree (linux_kernel_modules_nodes);
+  }
+
+  if (!have_generic) {
+   char dwait;
+   char **modules = linux_kernel_modules_get_by_subsystem ("generic", &dwait);
+
+   if (modules) {
+    pthread_t *threadid = emalloc (sizeof (pthread_t));
+
+    if (ethread_create (threadid, NULL, (void *(*)(void *))linux_kernel_modules_load, modules)) {
+     linux_kernel_modules_load (modules);
+    } else {
+     if (dwait)
+      threads = (pthread_t **)setadd ((void **)threads, threadid, SET_NOALLOC);
+    }
+   }
+  }
+
+  if (!have_audio) {
+   char dwait;
+   char **modules = linux_kernel_modules_get_by_subsystem ("audio", &dwait);
+
+   if (modules) {
+    pthread_t *threadid = emalloc (sizeof (pthread_t));
+
+    if (ethread_create (threadid, NULL, (void *(*)(void *))linux_kernel_modules_load, modules)) {
+     linux_kernel_modules_load (modules);
+    } else {
+     if (dwait)
+      threads = (pthread_t **)setadd ((void **)threads, threadid, SET_NOALLOC);
+    }
+   }
+  }
+ }
+
+ if (threads) {
+  int i = 0;
+
+  for (; threads[i]; i++) {
+   pthread_join (*(threads[i]), NULL);
+
+   free (threads[i]);
+  }
+
+  free (threads);
+ }
+
+ return status_ok;
+}
+
+#endif
+
 void linux_kernel_modules_boot_event_handler (struct einit_event *ev) {
  switch (ev->type) {
-  case einit_boot_load_kernel_extensions:
-   linux_kernel_modules_run(0);
+  case einit_boot_early:
+   linux_kernel_modules_run(lkm_pre_dev);
+   break;
 
-   /* some code */
+  case einit_boot_load_kernel_extensions:
+   linux_kernel_modules_run(lkm_post_dev);
    break;
 
    default: break;
@@ -249,7 +418,10 @@ int linux_kernel_modules_cleanup (struct lmodule *this) {
  exec_cleanup (this);
 
  event_ignore (einit_event_subsystem_boot, linux_kernel_modules_boot_event_handler);
+
+#if 0
  event_ignore (einit_event_subsystem_power, linux_kernel_modules_power_event_handler);
+#endif
 
  return 0;
 }
@@ -261,7 +433,10 @@ int linux_kernel_modules_configure (struct lmodule *this) {
  thismodule->cleanup = linux_kernel_modules_cleanup;
 
  event_listen (einit_event_subsystem_boot, linux_kernel_modules_boot_event_handler);
+
+#if 0
  event_listen (einit_event_subsystem_power, linux_kernel_modules_power_event_handler);
+#endif
 
  return 0;
 }
