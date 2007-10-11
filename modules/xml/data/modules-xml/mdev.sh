5530f5254bf0edbd85987fc9b76ee7124e2979e9
rmh3093 <rmh3093@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-10-11 18:32:53 +0000

    Add custom hotplug scripts for einit, either we can use these or listen to netlink, edited mdev to use custom hotplug scripts
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@2217 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- modules/xml/data/modules-xml/mdev.sh
+++ modules/xml/data/modules-xml/mdev.sh
@@ -2,6 +2,7 @@
 # eINIT mdev script
 
 if [ $1 = "enable" ]; then
+ echo '/lib/einit/scripts/einit-hotplug' > /proc/sys/kernel/hotplug
  mount -n -t tmpfs -o exec,nosuid,mode=0755 mdev /dev
  mdev -s
  chmod 777 /dev/null
