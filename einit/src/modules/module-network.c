cedce9d26f2a4098018ab31f8487854c55509b6d
rmh3093 <rmh3093@ce7afbd9-0f25-0410-bca2-9054d3de1fdb>
2007-10-20 00:01:31 +0000

    100ms :)
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/einit/trunk@2287 ce7afbd9-0f25-0410-bca2-9054d3de1fdb

--- einit/src/modules/module-network.c
+++ einit/src/modules/module-network.c
@@ -666,7 +666,7 @@ int network_ready (struct interface_descriptor *id, struct einit_event *status) 
    return status_failed;
   }
 
-  usleep(10000);
+  usleep(100000);
   retries++;
  }
  return ret;
