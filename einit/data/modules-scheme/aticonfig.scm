6e4968e72a5420e65fcc0043e10b72b3f0ba74fd
Magnus Deininger <mdeininger@celes.local>
2007-11-22 17:09:23 +0100

    updated this module a bit (thanks hkbst =))

--- einit/data/modules-scheme/aticonfig.scm
+++ einit/data/modules-scheme/aticonfig.scm
@@ -2,28 +2,20 @@
 ;;; aticonfig wrapper
 ;;;;;;;
 
-(letrec 
- ((check-aticonfig (lambda (node)
-   (if (null? node)
-       #f
-       (if (and
-            (string=? (car (car node)) "b")
-            (string=? (cdr (car node)) "yes"))
-           #t
-           (check-aticonfig (cdr node)))))))
+(letrec
+  ((check-aticonfig (lambda ()
+     (let ((a (assq "b" (get-configuration 'configuration-services-aticonfig-enable))))
+          (if a (string=? a "yes") #f)))))
 
- (if (check-aticonfig (get-configuration 'configuration-services-aticonfig-enable))
-     (begin
-       (make-module 's-aticonfig "ATIConfig Wrapper")
+  (if (check-aticonfig)
+      (begin
+        (make-module 's-aticonfig "ATIConfig Wrapper")
 
-       (letrec
-         ((get-aticonfig-options (lambda (node)
-           (if (null? node)
-               ""
-               (if (string=? (car (car node)) "s")
-                   (cdr (car node))
-                   (get-aticonfig-options (cdr node)))))))
+        (letrec
+          ((get-aticonfig-options (lambda ()
+            (let ((a (assq "s" (get-configuration 'configuration-services-aticonfig-options)))
+                 (if a (cdr a) #f))))))
 
-         (event-listen 'any
-           (lambda (event)
-             (shell (string-append "aticonfig " (get-aticonfig-options (get-configuration 'configuration-services-aticonfig-options))))))))))
+          (event-listen 'core/mode-switching
+            (lambda (event)
+              (shell (string-append "aticonfig " (get-aticonfig-options)))))))))
