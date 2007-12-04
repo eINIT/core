aac231d6e83102b17e87d4ab7236463d6c72e2fb
Magnus Deininger <mdeininger@celes.local>
2007-12-04 15:27:02 +0100

    added set-prebuffer, split set-lowmem into set-lowmem + set-common (the -prebuffer variant can be used to replace the -lowmem variant; it's based on pre-allocated chunks and should generally be faster and do a lot less malloc(), but it does waste some memory)

--- src/set-prebuffer.c
+++ src/set-prebuffer.c
@@ -48,480 +48,234 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <limits.h>
 #include <sys/types.h>
 
+#define SET_POINTERS 32
+#define SET_CHUNKSIZE 1024
+
 /* some common functions to work with null-terminated arrays */
 
-void **setcombine (const void **set1, const void **set2, const int32_t esize) {
- void **newset = NULL;
- int x = 0;
-
- if (!set1) return setdup(set2, esize);
- if (!set2) return setdup(set1, esize);
-
- for (x = 0; set1[x]; x++) {
-  if (!inset ((const void **)newset, set1[x], esize)) {
-   newset = setadd (newset, set1[x], esize);
-  }
- }
- for (x = 0; set2[x]; x++) {
-  if (!inset ((const void **)newset, set2[x], esize)) {
-   newset = setadd (newset, set2[x], esize);
-  }
- }
-
- return newset;
-}
-
-void **setcombine_nc (void **set1, const void **set2, const int32_t esize) {
- int x = 0;
-
- if (!set1) return setdup(set2, esize);
- if (!set2) return set1;
-
- for (x = 0; set2[x]; x++) {
-  if (!inset ((const void **)set1, set2[x], esize)) {
-   set1 = setadd (set1, set2[x], esize);
-  }
- }
-
- return set1;
-}
-
-void **setslice (const void **set1, const void **set2, const int32_t esize) {
- void **newset = NULL;
- int x = 0;
-
- if (!set1) return NULL;
- if (!set2) return setdup(set1, esize);
-
- for (; set1[x]; x++) {
-  if (!inset (set2, set1[x], esize)) {
-   newset = setadd (newset, set1[x], esize);
-  }
- }
-
- return newset;
-}
-
-void **setslice_nc (void **set1, const void **set2, const int32_t esize) {
- void **newset = NULL;
- int x = 0;
-
- if (!set1) return NULL;
- if (!set2) return set1;
-
- for (; set1[x]; x++) {
-  if (!inset (set2, set1[x], esize)) {
-   newset = setadd (newset, set1[x], esize);
-  }
- }
-
- efree (set1);
-
- return newset;
-}
+struct chunk_data_block {
+ size_t real_size;
+};
 
 void **setadd (void **set, const void *item, int32_t esize) {
- void **newset = NULL;
- int x = 0;
- uint32_t count = 0;
- uintptr_t size = 0;
+ if (!item) return set;
+ else {
+  void **newset = NULL;
+  char need_reindex = 0;
+  size_t size = 0, oldsize = 0;
+  int elements = 0, i = 0;
+  ssize_t indexsize = 0, oldindexsize = 0;
+  uintptr_t c = 0;
+  div_t dt;
+  struct chunk_data_block *db = NULL;
 
- if (!item) return NULL;
-// if (!set) set = ecalloc (1, sizeof (void *));
-
- if (esize == SET_NOALLOC) {
-  if (set) for (; set[count]; count++);
-  else count = 1;
-  size = (count+2)*sizeof(void*);
-
-  newset = ecalloc (1, size);
-
+/* get the new exact number of elements, i.e. elements+1 */
   if (set) {
-   while (set[x]) {
-    if (set[x] == item) {
-     efree (newset);
-     return set;
-    }
-    newset [x] = set[x];
-    x++;
+   do { elements++; } while (set[elements]);
+
+/* if we have SET_POINTERS*n new elements, that means we had SET_POINTERS-1 + the NULL before,
+   so we need to reindex */
+   if ((elements % SET_POINTERS) == 0)
+    need_reindex = 1;
+
+   elements++;
+
+   dt = div (elements +1, SET_POINTERS);
+   indexsize = (dt.quot + ((dt.rem == 0) ? 0 : 1)) * SET_POINTERS * sizeof(void *);
+//   fprintf (stderr, "index blocks (%i elements): %i (%i)\n", elements, (dt.quot + ((dt.rem == 0) ? 0 : 1)), indexsize);
+
+   dt = div (elements, SET_POINTERS);
+   oldindexsize = (dt.quot + ((dt.rem == 0) ? 0 : 1)) * SET_POINTERS * sizeof(void *);
+//   fprintf (stderr, "old index blocks: %i (%i)\n", (dt.quot + ((dt.rem == 0) ? 0 : 1)), oldindexsize);
+
+   indexsize += sizeof (struct chunk_data_block);
+   oldindexsize += sizeof (struct chunk_data_block);
+
+   db = ((char *)set) + oldindexsize - sizeof (struct chunk_data_block);
+
+#if 1
+   oldsize = db->real_size;
+
+   switch (esize) {
+    case SET_NOALLOC:
+     size = indexsize;
+     break;
+    case SET_TYPE_STRING:
+     size = (db->real_size - oldindexsize + indexsize) + strlen  (item) + 1;
+     break;
+    default:
+     size = indexsize + elements * esize;
+     break;
    }
-   efree (set);
-  }
 
-  newset[x] = (void *)item;
- } else if (esize == SET_TYPE_STRING) {
-  char *cpnt;
-  uint32_t strlen_item = 1+strlen(item);
+//   fprintf (stderr, "sizes: %i, %i (type=%i)", oldsize, size, esize);
+//   fflush (stderr);
+#else
+   oldsize = oldindexsize;
+   size = indexsize;
 
-  if (set) for (; set[count]; count++);
+   switch (esize) {
+    case SET_NOALLOC: break;
+    case SET_TYPE_STRING:
+     for (i = 0; set[i]; i++) {
+//      fprintf (stderr, "count %s\n", set[i]);
+//      fflush (stderr);
+	  size_t l = strlen  (set[i]) + 1;
 
-  if (count) {
-   uint32_t strlencache[count]/*, cpp = 0*/;
-/*   ssize_t copyblock_size[count+1];
-   void *copyblock_ptr[count+1];
+      size += l;
+      oldsize += l;
+	 }
 
-   copyblock_size[0] = 0;
-   copyblock_ptr[0] = NULL;*/
-
-   for (count = 0; set[count]; count++) {
-    size += sizeof(void*) + (strlencache[count] = (1+strlen(set[count])));
-
-/*    if (copyblock_size[cpp]) {
-    } else {*/
-/*    {
-     copyblock_ptr[cpp] = set[count];
-     copyblock_size[cpp] = strlencache[count];
-     cpp++;
-    }*/
+     size += strlen  (item) + 1;
+     break;
+    default:
+     oldsize += (elements -1) * esize;
+     size += elements * esize;
+     break;
    }
-   size += sizeof(void*)*2 + strlen_item;
+#endif
 
-   newset = ecalloc (1, size);
-   cpnt = ((char *)newset) + (count+2)*sizeof(void*);
-
-   while (set[x]) {
-/*    if (set[x] == item) {
-     efree (newset);
-     return set;
-    }*/
-    memcpy (cpnt, set[x], strlencache[x]);
-    newset [x] = cpnt;
-    cpnt += strlencache[x];
-    x++;
-   }
-   efree (set);
+   need_reindex = (oldindexsize != indexsize);
   } else {
-   newset = ecalloc (1, sizeof(void*)*2 + strlen_item);
-   cpnt = ((char *)newset) + (count+2)*sizeof(void*);
-  }
+   elements = 1;
+   oldsize = 0;
+   indexsize = (SET_POINTERS * sizeof (void *)) + sizeof (struct chunk_data_block);
+//   fprintf (stderr, "index blocks: 1 (%i)\n", indexsize);
 
-  memcpy (cpnt, item, strlen_item);
-  newset [x] = cpnt;
- } else {
-  char *cpnt;
-
-  if (set) for (; set[count]; count++) {
-   size += sizeof(void*) + esize;
-  }
-  size += sizeof(void*)*2 + esize;
-
-  newset = ecalloc (1, size);
-  cpnt = ((char *)newset) + (count+2)*sizeof(void*);
-
-  if (set) {
-   while (set[x]) {
-    if (set[x] == item) {
-     efree (newset);
-     return set;
-    }
-    memcpy (cpnt, set[x], esize);
-    newset [x] = cpnt;
-    cpnt += esize;
-    x++;
+   switch (esize) {
+    case SET_NOALLOC:
+     size = indexsize;
+     break;
+    case SET_TYPE_STRING:
+     size = indexsize + strlen (item) + 1;
+     break;
+    default:
+     size = indexsize + esize;
+     break;
    }
-   efree (set);
   }
 
-  memcpy (cpnt, item, esize);
-  newset [x] = cpnt;
-//  cpnt += esize;
- }
+/* at this point we got the minimum sizes we need... so we need to make that a multiple of SET_CHUNKSIZE*/
+  dt = div (size, SET_CHUNKSIZE);
+//  size = (dt.quot + ((dt.rem == 0) ? 0 : 1)) * SET_CHUNKSIZE;
+//  fprintf (stderr, "chunks: %i (%i)\n", (dt.quot + ((dt.rem == 0) ? 0 : 1)), size);
 
- return newset;
+  dt = div (oldsize, SET_CHUNKSIZE);
+  oldsize = (dt.quot + ((dt.rem == 0) ? 0 : 1)) * SET_CHUNKSIZE;
+//  fprintf (stderr, "old chunks: %i (%i)\n", (dt.quot + ((dt.rem == 0) ? 0 : 1)), oldsize);
+//  fflush (stderr);
+
+  if (need_reindex || (size != oldsize)) {
+   newset = emalloc (size);
+   memset (newset, 0, size);
+//   fprintf (stderr, "resize %i (block @%x)\n", size, newset);
+//   fflush (stderr);
+
+   db = ((char *)newset) + indexsize - sizeof (struct chunk_data_block);
+   db->real_size = indexsize;
+
+//   fprintf (stderr, "cdb @%x\n", db);
+//   fflush (stderr);
+
+   if (set) {
+//    fprintf (stderr, "copy\n");
+    switch (esize) {
+     case SET_NOALLOC:
+      for (i = 0; set[i]; i++) {
+	   newset[i] = set[i];
+      }
+      break;
+     case SET_TYPE_STRING:
+      c = (uintptr_t)(((char *)newset) + indexsize);
+      for (i = 0; set[i]; i++) {
+	   int l = strlen (set[i]) + 1;
+
+	   newset[i] = (void *)c;
+	   memcpy (newset[i], set[i], l);
+	   c += l;
+       db->real_size += l;
+	  }
+
+      break;
+     default:
+      for (i = 0; set[i]; i++) {
+	   newset[i] = ((char *)newset) + indexsize + (i * esize);
+	   memcpy (newset[i], set[i], esize);
+       db->real_size += esize;
+	  }
+      break;
+    }
+
+    free (set);
+   }
+
+   set = newset;
+  } else {
+   newset = set;
+  }
+
+  switch (esize) {
+   case SET_NOALLOC:
+    newset[elements -1] = (void *)item;
+    break;
+   case SET_TYPE_STRING:
+    c = (uintptr_t)(((char *)newset) + indexsize);
+    for (i = 0; newset[i]; i++) {
+	 size_t l = strlen (newset[i]) + 1;
+     c += l;
+    }
+
+    newset[i] = (void *)c;
+	{
+	 size_t l = strlen (item) + 1;
+     memcpy (newset[i], item, l);
+     db->real_size += l;
+	}
+    break;
+   default:
+    i = elements - 1;
+    newset[i] = ((char *)newset) + indexsize + (i * esize);
+    memcpy (newset[i], item, esize);
+    db->real_size += esize;
+    break;
+  }
+
+  return newset;
+ }
 }
 
 void **setdup (const void **set, int32_t esize) {
- void **newset;
- uint32_t y = 0, count = 0, size = 0;
+ void **newset = NULL;
+ uint32_t y = 0;
  if (!set) return NULL;
- if (!set[0]) return NULL;
 
- if (esize == -1) {
-  newset = ecalloc (setcount(set) +1, sizeof (char *));
-  while (set[y]) {
-   newset[y] = (void *)set[y];
-   y++;
-  }
- } else if (esize == 0) {
-  char *cpnt;
-
-  for (; set[count]; count++)
-   size += sizeof(void*) + 1 + strlen(set[count]);
-  size += sizeof(void*)*2;
-
-  newset = ecalloc (1, size);
-  cpnt = ((char *)newset) + (count+1)*sizeof(void*);
-
-  while (set[y]) {
-   esize = 1+strlen(set[y]);
-   memcpy (cpnt, set[y], esize);
-   newset [y] = cpnt;
-   cpnt += esize;
-   y++;
-  }
- } else {
-  char *cpnt;
-
-  for (; set[count]; count++)
-   size += sizeof(void*) + esize;
-  size += sizeof(void*)*2;
-
-  newset = ecalloc (1, size);
-  cpnt = ((char *)newset) + (count+1)*sizeof(void*);
-
-  while (set[y]) {
-   memcpy (cpnt, set[y], esize);
-   newset [y] = cpnt;
-   cpnt += esize;
-   y++;
-  }
- }
+ for (; set[y]; y++)
+  newset = setadd (newset, set[y], esize);
 
  return newset;
-}
-
-void **setdel (void **set, const void *item) {
- void **newset = set;
- int x = 0, y = 0;
-
- if (!item || !set) return NULL;
-
- while (set[y]) {
-  if (set[y] != item) {
-   newset [x] = set[y];
-   x++;
-  }
-  y++;
-/*  else {
-   set = set+1;
-  }*/
- }
-
- if (!x) {
-  efree (set);
-  return NULL;
- }
-
- newset[x] = NULL;
-
- return newset;
-}
-
-int setcount (const void **set) {
- int i = 0;
- if (!set) return 0;
- if (!set[0]) return 0;
- while (set[i])
-  i++;
-
- return i;
-}
-
-signed int sortfunction_lexical (const void *a, const void *b) {
- return -1 * strcmp (a, b);
-}
-
-/* not exactly efficient, but it works, better than before */
-void setsort (void **set, enum set_sort_order task, signed int(*sortfunction)(const void *, const void*)) {
- uint32_t i = 0;
-
- if (!set || !set[0] || !set[1]) return; // need a set with at least two elements to do anything meaningful.
-
- if (task == set_sort_order_string_lexical)
-  sortfunction = sortfunction_lexical;
- else if (!sortfunction) return;
-
- for (;set[i];i++) {
-  uint32_t j = i;
-  char ex = 0;
-
-  for (;set[j];j++) {
-   if ((ex = (sortfunction(set[i], set[j]) < 0))) break;
-  }
-
-  if (ex) {
-   void *cur = set[i];
-
-   uint32_t k = i+1;
-   for (;set[k];k++) {
-    set[k-1] = set[k];
-   }
-
-   set[k-1] = cur;
-
-   i--;
-  }
- }
-}
-
-int inset (const void **haystack, const void *needle, int32_t esize) {
- int c = 0;
-
- if (!haystack) return 0;
- if (!haystack[0]) return 0;
- if (!needle) return 0;
-
- if (esize == SET_TYPE_STRING) {
-  for (; haystack[c] != NULL; c++)
-   if (strmatch (haystack[c], needle)) return 1;
- } else if (esize == -1) {
-  for (; haystack[c] != NULL; c++)
-   if (haystack[c] == needle) return 1;
- }
- return 0;
-}
-
-#ifdef POSIXREGEX
-#include <regex.h>
-#endif
-
-char ** inset_pattern (const void **haystack, const void *needle, int32_t esize) {
-#ifdef POSIXREGEX
- regex_t pattern;
-#endif
- int c = 0;
- char **retval = NULL;
-
- if (!haystack) return 0;
- if (!haystack[0]) return 0;
- if (!needle) return 0;
-
- if (esize == SET_TYPE_STRING) {
-#ifdef POSIXREGEX
-  if (eregcomp (&pattern, needle)) {
-#endif
-   for (; haystack[c] != NULL; c++)
-    if (strmatch (haystack[c], needle))
-     retval = (char **)setadd ((void **)retval, haystack[c], SET_TYPE_STRING);
-#ifdef POSIXREGEX
-  } else {
-   for (; haystack[c] != NULL; c++)
-    if (!regexec (&pattern, haystack[c], 0, NULL, 0))
-     retval = (char **)setadd ((void **)retval, haystack[c], SET_TYPE_STRING);
-
-   regfree (&pattern);
-  }
-#endif
- } else if (esize == -1) {
-  for (; haystack[c] != NULL; c++)
-   if (haystack[c] == needle)
-    retval = (char **)setadd ((void **)retval, haystack[c], SET_TYPE_STRING);
- }
-
- return retval;
 }
 
 /* some functions to work with string-sets */
 
 char **str2set (const char sep, const char *oinput) {
- int l, i = 0, sc = 1, cr = 1;
- char **ret;
- char *input;
+ int i;
+ char *input, *li, **ret = NULL;
  if (!oinput || !(input = estrdup (oinput))) return NULL;
- l = strlen (input)-1;
 
- for (; i < l; i++) {
-  if (input[i] == sep) {
-   sc++;
-//   input[i] = 0;
-  }
- }
- ret = ecalloc (1, ((sc+1)*sizeof(char *)) + 3 + l);
- memcpy ((((char *)ret) + ((sc+1)*sizeof(char *))), input, 2 + l);
- efree (input);
- input = (char *)(((char *)ret) + ((sc+1)*sizeof(char *)));
- ret[0] = input;
- for (i = 0; i < l; i++) {
-  if (input[i] == sep) {
-   ret[cr] = input+i+1;
-   input[i] = 0;
-   cr++;
-  }
- }
- return ret;
-}
-
-char *set2str (const char sep, const char **input) {
- char *ret = NULL;
- size_t slen = 0;
- uint32_t i = 0;
- char nsep[2] = {sep, 0};
-
- if (!input) return NULL;
-
- for (; input[i]; i++) {
-  slen += strlen (input[i])+1;
- }
-
- ret = emalloc (slen);
- *ret = 0;
+ li = input;
 
  for (i = 0; input[i]; i++) {
-  if (i != 0)
-   strcat (ret, nsep);
-
-  strcat (ret, input[i]);
+  if (input[i] == sep) {
+   input[i] = 0;
+   if (li[0] != 0)
+    ret = (char **)setadd ((void **)ret, li, SET_TYPE_STRING);
+   li = input + i +1;
+   input[i] = sep;
+  }
  }
+ if (li[0] != 0)
+  ret = (char **)setadd ((void **)ret, li, SET_TYPE_STRING);
 
+ efree (input);
  return ret;
-}
-
-char **strsetdel (char **set, char *item) {
- char **newset = set;
- int x = 0, y = 0;
-
- if (!item || !set) return NULL;
- if (!set[0]) {
-  efree (set);
-  return NULL;
- }
-
- while (set[y]) {
-  if (strcmp(set[y], item)) {
-   newset [x] = set[y];
-   x++;
-  }
-  y++;
-/*  else {
-   set = set+1;
-  }*/
- }
-
- if (!x) {
-  efree (set);
-  return NULL;
- }
-
- newset[x] = NULL;
-
- return newset;
-}
-
-char **strsetdeldupes (char **set) {
- char **newset = set;
- int x = 0, y = 0;
-
- if (!set) return NULL;
-
- while (set[y]) {
-  char *tmp = set[y];
-  set[y] = NULL;
-  if (!inset ((const void **)set, (const void *)tmp, SET_TYPE_STRING)) {
-   newset [x] = tmp;
-   x++;
-  }
-  y++;
-/*  else {
-   set = set+1;
-  }*/
- }
-
- if (!x) {
-  efree (set);
-  return NULL;
- }
-
- newset[x] = NULL;
-
- return newset;
 }
