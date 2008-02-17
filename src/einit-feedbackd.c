7f6140315646df3b28bbbf2bfe76c17741393b04
Magnus Deininger <dma05@web.de>
2008-02-16 23:32:34 -0500

    stupid lil output thingy

--- src/einit-feedbackd.c
+++ src/einit-feedbackd.c
@@ -50,10 +50,111 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <einit/configuration-static.h>
 
 #include <einit/einit.h>
+#include <einit/tree.h>
 
 #include <curses.h>
 
+struct module_status {
+ enum einit_module_status status;
+};
+
+#define attr_red 1
+#define attr_blue 2
+#define attr_green 3
+#define attr_yellow 4
+
+struct stree *status_tree = NULL;
+
+void set_module_status (char *name, enum einit_module_status status) {
+ struct stree *e;
+ if (!status_tree || !(e = streefind (status_tree, name, tree_find_first))) {
+  struct module_status s;
+
+  s.status = status;
+  status_tree = streeadd (status_tree, name, &s, sizeof (s), NULL);
+ } else {
+  struct module_status *s = e->value;
+
+  s->status = status;
+ }
+}
+
+void move_to_right_border () {
+ int x, y, maxx, maxy;
+
+ getyx (stdscr, y, x);
+ getmaxyx(stdscr, maxy, maxx);
+
+ move (y, maxx - 13);
+}
+
 void update() {
+ erase();
+ if (status_tree) {
+  struct stree *st = streelinear_prepare (status_tree);
+
+  while (st) {
+   struct module_status *s = st->value;
+
+   /* "[  working ]" */
+   /* "[  failed  ]" */
+   /* "[  enabled ]" */
+   /* "[ diasbled ]" */
+
+   char buffer[BUFFERSIZE];
+   if (s->status & status_working) {
+    snprintf (buffer, BUFFERSIZE, " :: %s", st->key);
+    addstr (buffer);
+
+    move_to_right_border();
+    addstr ("[  ");
+
+    attron(COLOR_PAIR(attr_blue));
+    addstr ("working");
+    attroff(COLOR_PAIR(attr_blue));
+
+    addstr (" ]\n");
+   } else if (s->status & status_failed) {
+    snprintf (buffer, BUFFERSIZE, " :: %s", st->key);
+    addstr (buffer);
+
+    move_to_right_border();
+    addstr ("[  ");
+
+    attron(COLOR_PAIR(attr_red));
+    addstr ("failed");
+    attroff(COLOR_PAIR(attr_red));
+
+    addstr ("  ]\n");
+   } else if (s->status & status_enabled) {
+    snprintf (buffer, BUFFERSIZE, " :: %s", st->key);
+    addstr (buffer);
+
+    move_to_right_border();
+    addstr ("[  ");
+
+    attron(COLOR_PAIR(attr_green));
+    addstr ("enabled");
+    attroff(COLOR_PAIR(attr_green));
+
+    addstr (" ]\n");
+   } else if (s->status & status_enabled) {
+    snprintf (buffer, BUFFERSIZE, " :: %s", st->key);
+    addstr (buffer);
+
+    move_to_right_border();
+    addstr ("[ ");
+
+    attron(COLOR_PAIR(attr_yellow));
+    addstr ("disabled");
+    attroff(COLOR_PAIR(attr_yellow));
+
+    addstr (" ]\n");
+   }
+
+   st = streenext (st);
+  }
+ }
  refresh();
 }
 
@@ -75,25 +176,14 @@ void event_handler_mode_switch_done (struct einit_event *ev) {
 }
 
 void event_handler_update_module_status (struct einit_event *ev) {
- char buffer[BUFFERSIZE];
- if (ev->status & status_working) {
-  snprintf (buffer, BUFFERSIZE, "now working on: %s\n", ev->rid);
-  addstr (buffer);
- } else if (ev->status & status_failed) {
-  snprintf (buffer, BUFFERSIZE, "manipulation failed: %s\n", ev->rid);
-  addstr (buffer);
- } else if (ev->status & status_enabled) {
-  snprintf (buffer, BUFFERSIZE, "now enabled: %s\n", ev->rid);
-  addstr (buffer);
- } else if (ev->status & status_enabled) {
-  snprintf (buffer, BUFFERSIZE, "now disabled: %s\n", ev->rid);
-  addstr (buffer);
+ if (ev->rid) {
+  set_module_status (ev->rid, ev->status);
  }
 
- if (ev->string) {
+/* if (ev->string) {
   snprintf (buffer, BUFFERSIZE, " > %s: %s\n", ev->rid, ev->string);
   addstr (buffer);
- }
+ }*/
 
  update();
 }
@@ -116,8 +206,14 @@ void event_handler_update_service_disabled (struct einit_event *ev) {
 
 int main(int argc, char **argv, char **env) {
  initscr();
+ start_color();
  cbreak();
  noecho();
+
+ init_pair(attr_red, COLOR_RED, COLOR_BLACK);
+ init_pair(attr_blue, COLOR_BLUE, COLOR_BLACK);
+ init_pair(attr_green, COLOR_GREEN, COLOR_BLACK);
+ init_pair(attr_yellow, COLOR_YELLOW, COLOR_BLACK);
 
  nonl();
  intrflush(stdscr, FALSE);
