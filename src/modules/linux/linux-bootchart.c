28882913f9553ce9b298895732e998f31b8a9a5c
Magnus Deininger <dma05@web.de>
2008-03-04 08:21:58 +0000

    this oughta start the bootchart earlier (more reliably, that is)

--- src/modules/linux/linux-bootchart.c
+++ src/modules/linux/linux-bootchart.c
@@ -472,7 +472,7 @@ int linux_bootchart_configure (struct lmodule *tm) {
  }
 
 
- event_listen (einit_boot_early, linux_bootchart_boot_event_handler);
+ event_listen (einit_boot_load_kernel_extensions, linux_bootchart_boot_event_handler);
  event_listen (einit_core_mode_switching, linux_bootchart_switch);
  event_listen (einit_core_done_switching, linux_bootchart_switch_done);
 
