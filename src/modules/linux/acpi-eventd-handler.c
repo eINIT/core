c410ecb752f99089eca59c1a4730cbff5d4bdd24
Ryan Hope <rmh3093@gmail.com>
2008-03-09 03:25:38 -0400

    cant find magnus's last commits

---
+++ src/modules/linux/acpi-eventd-handler.c
@@ -0,0 +1,67 @@
+/*
+ *  acpi-eventd-handler.c
+ *  einit
+ *
+ *  Created on 03/09/2008.
+ *  Copyright 2008 Ryan Hope. All rights reserved.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <einit/module.h>
+#include <einit/config.h>
+#include <einit/bitch.h>
+#include <einit/utility.h>
+#include <einit/event.h>
+#include <errno.h>
+#include <syslog.h>
+
+#define EXPECTED_EIV 1
+
+#if EXPECTED_EIV != EINIT_VERSION
+#warning "This module was developed for a different version of eINIT, you might experience problems"
+#endif
+
+int acpi_eventd_handler_configure (struct lmodule *);
+
+#if defined(EINIT_MODULE) || defined(EINIT_MODULE_HEADER)
+
+char * acpi_eventd_handler_provides[] = {"acpi-eventd-handler", NULL};
+char * acpi_eventd_handler_requires[] = {"acpi-eventd", NULL};
+
+/* no const here, we need to mofiy this on the fly */
+
+struct smodule acpi_eventd_handler_self = {
+		.eiversion = EINIT_VERSION,
+		.eibuild   = BUILDNUMBER,
+		.version   = 1,
+		.mode      = einit_module,
+		.name      = "ACPI Eventd Handler",
+		.rid       = "acpi-eventd-handler",
+		.si        = {
+				.provides = acpi_eventd_handler_provides,
+				.requires = acpi_eventd_handler_requires,
+				.after    = NULL,
+				.before   = NULL
+		},
+		.configure = acpi_eventd_handler_configure
+};
+
+module_register(acpi_eventd_handler_self);
+
+#endif
+
+void acpi_eventd_handler_generic_event_handler(struct einit_event *ev) {
+	syslog(LOG_INFO,"heard event");
+}
+
+int acpi_eventd_handler_configure (struct lmodule *pa) {
+	module_init (pa);
+	event_listen (einit_acpi_event_generic, acpi_eventd_handler_generic_event_handler);
+	return 0;
+}
+
