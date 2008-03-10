b363d2d30fc079359c3ce63714f586dc2061b06f
Ryan Hope <rmh3093@gmail.com>
2008-03-10 12:51:31 -0400

    make acpi event handler dump to syslog, test config tree walking

--- src/modules/linux/acpi-eventd-handler.c
+++ src/modules/linux/acpi-eventd-handler.c
@@ -17,8 +17,11 @@
 #include <einit/bitch.h>
 #include <einit/utility.h>
 #include <einit/event.h>
+#include <einit/tree.h>
 #include <errno.h>
 #include <syslog.h>
+
+#define ACPI_EVENT_PREFIX "configuration-services-acpi-eventd-event"
 
 #define EXPECTED_EIV 1
 
@@ -26,7 +29,7 @@
 #warning "This module was developed for a different version of eINIT, you might experience problems"
 #endif
 
-int acpi_eventd_handler_configure (struct lmodule *);
+int acpi_eventd_handler_configure(struct lmodule *);
 
 #if defined(EINIT_MODULE) || defined(EINIT_MODULE_HEADER)
 
@@ -35,19 +38,19 @@ char * acpi_eventd_handler_provides[] = {"acpi-eventd-handler", NULL};
 /* no const here, we need to mofiy this on the fly */
 
 struct smodule acpi_eventd_handler_self = {
-		.eiversion = EINIT_VERSION,
-		.eibuild   = BUILDNUMBER,
-		.version   = 1,
-		.mode      = einit_module,
-		.name      = "ACPI Eventd Handler",
-		.rid       = "acpi-eventd-handler",
-		.si        = {
-				.provides = acpi_eventd_handler_provides,
-				.requires = NULL,
-				.after    = NULL,
-				.before   = NULL
-		},
-		.configure = acpi_eventd_handler_configure
+	.eiversion = EINIT_VERSION,
+	.eibuild = BUILDNUMBER,
+	.version = 1,
+	.mode = einit_module,
+	.name = "ACPI Eventd Handler",
+	.rid = "acpi-eventd-handler",
+	.si = {
+		.provides = acpi_eventd_handler_provides,
+		.requires = NULL,
+		.after = NULL,
+		.before = NULL
+	},
+	.configure = acpi_eventd_handler_configure
 };
 
 module_register(acpi_eventd_handler_self);
@@ -55,12 +58,29 @@ module_register(acpi_eventd_handler_self);
 #endif
 
 void acpi_eventd_handler_generic_event_handler(struct einit_event *ev) {
-	syslog(LOG_INFO,"heard event");
+	syslog(LOG_INFO, "%s\"%s\"[%d]",ev->rid,ev->string,ev->status);
+	struct stree *st;
+	st = cfg_prefix(ACPI_EVENT_PREFIX);
+	puts("a");
+	if (st) {
+		puts("b");
+		struct stree *cur = streelinear_prepare(st);
+		while (cur) {
+			puts("c");
+			struct cfgnode *n = cur->value;
+			if (n->arbattrs) {
+				puts("d");
+			}
+			cur = streenext(cur);
+		}
+		streefree(st);
+	}
 }
 
-int acpi_eventd_handler_configure (struct lmodule *pa) {
-	module_init (pa);
-	event_listen (einit_acpi_event_generic, acpi_eventd_handler_generic_event_handler);
+int acpi_eventd_handler_configure(struct lmodule *pa) {
+	module_init(pa);
+	event_listen(einit_acpi_event_generic,
+			acpi_eventd_handler_generic_event_handler);
 	return 0;
 }
 
