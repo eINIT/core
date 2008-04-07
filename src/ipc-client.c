e749554fbe25297b6fba5fa38b5ee1ba540b3059
Magnus Deininger <dma05@web.de>
2008-04-07 23:16:19 +0200

    mkay thats prolly it for the night

--- src/ipc-client.c
+++ src/ipc-client.c
@@ -60,6 +60,51 @@ struct einit_ipc_callback *einit_ipc_callbacks = NULL;
 
 struct einit_sexp_fd_reader *einit_ipc_client_rd = NULL;
 
+/* (define-record einit:event
+  type integer status task flag string stringset module)*/
+
+enum sexp_event_parsing_stage {
+ seps_type,
+ seps_integer,
+ seps_status,
+ seps_task,
+ seps_flag,
+ seps_string,
+ seps_stringset,
+ seps_module,
+ seps_done
+};
+
+void einit_ipc_handle_sexp_event (struct einit_sexp *sexp) {
+ enum sexp_event_parsing_stage s = seps_type;
+
+ while ((s != seps_done) && (sexp->type == es_cons)) {
+  switch (s) {
+   case seps_type:
+    break;
+   case seps_integer:
+    break;
+   case seps_status:
+    break;
+   case seps_task:
+    break;
+   case seps_flag:
+    break;
+   case seps_string:
+    break;
+   case seps_stringset:
+    break;
+   case seps_module:
+    break;
+
+   default:
+    break;
+  }
+
+  s++;
+ }
+}
+
 char einit_ipc_loop() {
  if (!einit_ipc_client_rd) return 0;
 
@@ -72,20 +117,31 @@ char einit_ipc_loop() {
    continue;
   }
 
-  if (einit_ipc_callbacks) {
-   void (*handler)(struct einit_sexp *) = einit_ipc_callbacks->handler;
-   struct einit_ipc_callback * t = einit_ipc_callbacks;
+  if (sexp->type == es_cons) {
+   if ((sexp->secundus->type == es_cons) && (sexp->secundus->secundus->type == es_cons) && (sexp->secundus->secundus->secundus->type == es_list_end)) { /* 3-tuple: must be a reply, or a request */
+    if ((sexp->primus->type == es_symbol) && (strmatch (sexp->primus->symbol, "reply"))) {
+     if (einit_ipc_callbacks) {
+      void (*handler)(struct einit_sexp *) = einit_ipc_callbacks->handler;
+      struct einit_ipc_callback * t = einit_ipc_callbacks;
 
-   einit_ipc_callbacks = t->next;
-   efree (t);
+      einit_ipc_callbacks = t->next;
+      efree (t);
 
-   handler(sexp);
+      handler(sexp);
+     }
+    }
+   } else {
+    if ((sexp->primus->type == es_symbol) && (strmatch (sexp->primus->symbol, "event"))) {
+     einit_ipc_handle_sexp_event (sexp->secundus);
+    }
+   }
+  } else {
+   char *r = einit_sexp_to_string (sexp);
+   fprintf (stderr, "BAD MESSAGE: %s\n", r);
+
+   efree (r);
   }
 
-  char *r = einit_sexp_to_string (sexp);
-  fprintf (stderr, "|%s|\n", r);
-
-  efree (r);
   einit_sexp_destroy (sexp);
  }
 
@@ -98,6 +154,8 @@ void einit_ipc_request_callback (const char *request, void (*handler)(struct ein
  struct einit_ipc_callback *cb = emalloc (sizeof (struct einit_ipc_callback));
  cb->next = einit_ipc_callbacks;
  cb->handler = handler;
+
+ einit_ipc_callbacks = cb;
 
  write (einit_ipc_client_rd->fd, request, strlen (request));
  fsync(einit_ipc_client_rd->fd);
@@ -115,7 +173,7 @@ char einit_ipc_connect (const char *address) {
  snprintf (addr.sun_path, sizeof(addr.sun_path), "%s", address);
 
  if (connect(fd, (const struct sockaddr *)&addr, sizeof(struct sockaddr_un)) < 0) {
-  perror ("bind()");
+  perror ("connect()");
   close (fd);
   return 0;
  }
